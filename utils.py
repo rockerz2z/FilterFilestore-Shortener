18b99f270f77c2671d84189a2f57b770071b28c8
NITEESH R <86245679+NiteeshRavichandran@users.noreply.github.com>
2023-05-18 22:07:16 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,7 +382,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://v2.kpslink.in/api'
+    url = f'https://v2.kpslink.in/st'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
