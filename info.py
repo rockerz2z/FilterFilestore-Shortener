714572e85a7f733b22974aebde9d18f90cd69868
Rocker2k <119720415+rockerz2z@users.noreply.github.com>
2024-04-10 19:22:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,8 +71,8 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
     
       # URL Shortener #
 
-URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'tnshort.net')
-URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'bfa604dd55a50e579d45089e700451bd38543b34')
+URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'publicearn.com')
+URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', '998ddd78dda4c6984ae045c0f21bc71ef09672f1)
 
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 120))
