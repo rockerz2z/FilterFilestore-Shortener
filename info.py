57c5a7905116203e78461eacae742038265575fc
Rocker2k <119720415+rockerz2z@users.noreply.github.com>
2024-11-28 17:10:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -40,7 +40,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002206464807'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002345121344'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'rockerz2z')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
